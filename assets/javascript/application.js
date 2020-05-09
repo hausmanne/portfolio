a32aae0c1a2594c19db7b8788d9bc8188d6bbd47
hausmanne <54555072+hausmanne@users.noreply.github.com>
2020-05-09 13:37:36 +0200

    Sticky nav added

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -5,3 +5,17 @@ function googleAnalytics() {
 
     gtag('config', 'UA-159516284-2');
 }
+
+function stickyNav() {
+    var yourNavigation = $(".navbar");
+    stickyDiv = "sticky";
+    yourHeader = $('#home').height();
+
+    $(window).scroll(function() {
+    if( $(this).scrollTop() > yourHeader ) {
+        yourNavigation.addClass(stickyDiv);
+    } else {
+        yourNavigation.removeClass(stickyDiv);
+    }
+    });
+}
